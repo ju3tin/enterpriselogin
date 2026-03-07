8b26222d2ba6bf9bd2bc8c348472e9479d04b0b0
ju3tin <ju3tin@hotmail.co.uk>
2026-03-07 13:38:04 +0000

    sdf

--- server.js
+++ server.js
@@ -1,31 +1,32 @@
-require("dotenv").config();
-
 const express = require("express");
 const cors = require("cors");
-const connectDB = require("./config/db");
 
-const authRoutes = require("./routes/auth");
+const connectDB = require("../config/db");
+const authRoutes = require("../routes/auth");
 
 const app = express();
 
+/* connect database */
 connectDB();
 
+/* CORS (allow all) */
 app.use(cors({
-  origin: "https://agenticenterprise.github.io",
-  methods: ["GET","POST","PUT","DELETE"],
+  origin: "*",
+  methods: ["GET","POST","PUT","DELETE","OPTIONS"],
   allowedHeaders: ["Content-Type","Authorization"]
 }));
 
+/* body parsing */
 app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
+/* routes */
 app.use("/api/auth", authRoutes);
 
+/* test route */
 app.get("/", (req,res)=>{
-  res.send("API Running");
+  res.json({message:"API running"});
 });
- 
-const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,()=>{
-  console.log(`Server running on port ${PORT}`);
-});
+/* export instead of listen */
+module.exports = app;
